8de61d9e82455f87ac103992c021f753bca78f3c
Chethana N P <chethana.np@cumulations.com>
2022-06-22 17:45:00 +0530

    changed from stateless to statefullWidget so that on tap question changes

--- app2/lib/main.dart
+++ app2/lib/main.dart
@@ -5,11 +5,32 @@ void main(){
 }
 //variables inside class are known as properties
 //functions inside class are known as methods
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+   
+    return MyAppState();
 
+     // throw UnimplementedError();
+  }
+}
+
+//here create a class with widget name ending with State
+//here this state will be persistent
+//State<MyApp> tells state belongs to MyApp
+class MyAppState extends State<MyApp> {
+var indexQuestion = 0;
 
   @override 
   Widget build(BuildContext context){
+    void answerQuestion(){
+      setState(() {
+        indexQuestion = indexQuestion + 1;
+      });
+       
+       print(indexQuestion);
+    }
     var questions = [
       'What\'s your favourite color?', 
       'City you would love to visit?', 
@@ -20,15 +41,20 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(title: Text('My First App'),
       ),
       body: Column(children: [
-        Text('The question!'),
+        Text(questions[indexQuestion],
+        ),
         RaisedButton(
-          onPressed: () => print('You tapped answe 1 !!'), child: Text('Answer 1'),
+          onPressed: answerQuestion, 
+          child: Text('Answer 1'),
           ),
         RaisedButton(
-          onPressed: () => print('You tapped answe 2 !!'), child: Text('Answer 2'),
+          onPressed: () => print('You tapped answe 2 !!'), 
+          child: Text('Answer 2'),
           ),
         RaisedButton(
-          onPressed:  () => print('You tapped answe 3 !!'), child: Text('Answer 3'),
+          onPressed:  () {
+            print('You tapped answe 3 !!');
+            } , child: Text('Answer 3'),
           ),
       ],
       ),
