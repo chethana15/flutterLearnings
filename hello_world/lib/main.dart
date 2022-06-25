82cfa2703e0a026bde98ff885cfbf7bc0ddf9f38
Chethana N P <chethana.np@cumulations.com>
2022-06-25 16:12:14 +0530

    made text to center

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 void main(){
 
-  runApp(
-    Text(
-    "Hello World!!",
-    textDirection: TextDirection.ltr,
-  ));
+   runApp(
+    Center(
+      child:  Text(
+              "Hello World!!",
+              textDirection: TextDirection.ltr,
+                  ) ,
+  )
+  
+  );
 }
